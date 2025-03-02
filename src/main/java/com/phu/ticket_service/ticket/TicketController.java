766930210aac004c22070718056f193c288763a7
phu-phurithat <phurithat.komkham@gmail.com>
2025-03-03 00:53:07 +0700

    add feature DnD

--- src/main/java/com/phu/ticket_service/ticket/TicketController.java
+++ src/main/java/com/phu/ticket_service/ticket/TicketController.java
@@ -5,15 +5,15 @@ import com.phu.ticket_service.ticket.model.TicketResponse;
 import com.phu.ticket_service.ticket.model.TicketUpdateRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/tickets")
+@CrossOrigin(origins = "http://localhost:5173")
 public class TicketController {
 
     private final TicketService ticketService;
@@ -24,9 +24,13 @@ public class TicketController {
     }
 
     @GetMapping
-    public ResponseEntity<List<TicketResponse>> getAllTickets (@RequestParam(defaultValue = "status") String sortBy,
-                                                              @RequestParam(defaultValue = "asc") String direction){
-        return ResponseEntity.ok(ticketService.findAllTickets(sortBy, direction));
+    public ResponseEntity<Page<TicketResponse>> getAllTickets (@RequestParam(defaultValue = "lastUpdate" ) String sortBy,
+                                                               @RequestParam (defaultValue = "desc" ) String direction,
+                                                               @RequestParam(defaultValue = "10") int pageSize,
+                                                               @RequestParam(defaultValue = "0") int page,
+                                                               @RequestParam(required = false) String status){
+        System.out.println(ticketService.findTicketsPageSortFilter(sortBy, direction,status, page, pageSize));
+        return ResponseEntity.ok(ticketService.findTicketsPageSortFilter(sortBy, direction,status, page, pageSize));
     }
 
     @PutMapping("/{id}")
@@ -34,8 +38,9 @@ public class TicketController {
         return ResponseEntity.ok(ticketService.updateTicket(id, request));
     }
 
-    @GetMapping("/status")
-    public ResponseEntity<List<TicketResponse>> getTicketsByStatus(@RequestParam String status){
-        return ResponseEntity.ok(ticketService.findTicketByStatus(status));
+    @PutMapping("/{id}/status")
+    public ResponseEntity<TicketResponse> updateTicketStatus(@PathVariable Long id, @RequestParam String status){
+        System.out.println(status);
+        return ResponseEntity.ok(ticketService.updateTicketStatus(id, status));
     }
 }
--- src/main/java/com/phu/ticket_service/ticket/TicketRepository.java
+++ src/main/java/com/phu/ticket_service/ticket/TicketRepository.java
@@ -2,12 +2,12 @@ package com.phu.ticket_service.ticket;
 
 import com.phu.ticket_service.ticket.model.Status;
 import com.phu.ticket_service.ticket.model.Ticket;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
-    List<Ticket> findByStatus(Status status);
+    Page<Ticket> findByStatus(Status status, Pageable pageable);
 }
